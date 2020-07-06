a28ad8e95011d6828c13d36156f3a3f53b31292e
tonyxu <tonyxu@cureray.cn>
2020-07-06 23:36:42 +0800

    typo correction

--- Offer32_levelOrder.py
+++ Offer32_levelOrder.py
@@ -2,21 +2,33 @@ from typing import List
 from collections import deque
 
 # Definition for a binary tree node.
- class TreeNode:
-     def __init__(self, x):
-         self.val = x
-         self.left = None
-         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
         if root is None:
-            return None
-        node_deque = deque()
-        node_deque.append(root)
-        while root.left or root.right:
-            node_deque.append([root.left, root.right])
-        return
+            return []
+        dq = deque()
+        dq.append(root)
+        result_list = []
+        while dq:
+            dq_length = len(dq)
+            tmp_list = []
+            while dq_length > 0:
+                dq_length -= 1
+                node = dq.popleft()
+                if node is not None:
+                    tmp_list += [node.val]
+                if node.left is not None:
+                    dq.append(node.left)
+                if node.right is not None:
+                    dq.append(node.right)
+            result_list.append(tmp_list)
+        return result_list
 
 
 # TODO: 使用广度优先搜索BFS解题
